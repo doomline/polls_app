ae5c618b76a331076aac5ffbc129085af83b5777
Alex <climberfields@gmail.com>
2020-09-04 07:22:10 -0500

    added the template loader to the views and introduced the concept of using templates to pull from. Added the index.html page to the template to use at the index page.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 # Create your views here.
-
+# views are a type of web page in the Django app
+# They will all have their own template as part of a specific function.
+# these will set a specific URL in place. The argument "request" is the request to the page (IE GET, POST ETC)
+# The argument question_id is the question id that is input
+# HTTP response is what the page will post
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
